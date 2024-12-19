359379c7cd78e57563bd4ee9719f6cb64ec549ca
JeisonJossa <jeisonjossa77@gmail.com>
2024-12-19 18:22:13 -0500

    Creacion de data en JS

---
+++ src/Data/Productos.js
@@ -0,0 +1,38 @@
+export const Productos = [
+  {
+    id: 1,
+    nombre: "Producto 1",
+    descripcion: "Descripción del producto 1",
+    precio: 100,
+  },
+  {
+    id: 2,
+    nombre: "Producto 2",
+    descripcion: "Descripción del producto 2",
+    precio: 200,
+  },
+  {
+    id: 3,
+    nombre: "Producto 3",
+    descripcion: "Descripción del producto 3",
+    precio: 300,
+  },
+  {
+    id: 4,
+    nombre: "Producto 4",
+    descripcion: "Descripción del producto 4",
+    precio: 400,
+  },
+  {
+    id: 5,
+    nombre: "Producto 5",
+    descripcion: "Descripción del producto 5",
+    precio: 500,
+  },
+  {
+    id: 6,
+    nombre: "Producto 6",
+    descripcion: "Descripción del producto 6",
+    precio: 600,
+  }
+];
